82315917fa018aa819d183d8129494333039c0b2
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-14 21:18:38 +0300

    Course model added, admin register, test course data.

--- course_service/models.py
+++ course_service/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(
+        max_length=60,
+        blank=False,
+        verbose_name="Name")
+    description = models.TextField(
+        blank=True,
+        default='',
+        verbose_name="Description")
+    start_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name= "Start date")
+    end_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name= "End date")
+    price = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+    def __str__(self):
+        return "{}".format(self.name)
