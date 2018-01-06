12db8359fea2c715018315a669a29a1514272ecc
Анастасия Пучнина <puchnina.nastya@gmail.com>
2018-01-06 20:08:07 +0300

    Exception handler not detail error bug fixed.

--- dips_course_service/settings.py
+++ dips_course_service/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '10m9!q@z2w=1aa6&q)$8kyeli(@39(uuh_f=$ho+gk_^$58o^x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
